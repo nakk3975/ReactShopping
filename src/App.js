76f2cd16a721446c41f95cc2f099ac176474349b
안재원 <dsfs3975@gmail.com>
2024-06-11 21:10:41 +0900

    [안재원]  - nav 추가  - 메인 이미지 추가  - 상품 이미지 3개 추가  - 이미지 저장 방식 메모

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import bg from './img/bg.png';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
+      <Navbar expand="lg" bg="light" variant='light'>
+        <Container>
+          <Navbar.Brand href="#home">won's Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      {/* 오리지널 BootStrap */}
+      {/* public 폴더 img 저장시 /img.png로 불러 올 수 있음 */}
+      {/* src={process.env.PUBLIC_URL + '/img/logo.png'} */}
+      {/* 이게 public 폴더 이미지 쓰는 권장 방식 */}
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
